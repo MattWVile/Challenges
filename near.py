9ea4a2864991c0748de986ae804da9aff8daa60c
MattWVile <matt.vile@yahoo.co.uk>
2021-07-06 10:35:39 +0000

    near function complete

--- near.py
+++ near.py
@@ -1,7 +1,11 @@
-firststring = input('first string please ')
-secondstring = input('second string please ')
-liststring = list(firststring)
-for index, letter in enumerate (liststring):
-    print(letter)
-    print(index)
-    // remove index, turn to string, compare against secondstring
+def near(first_string, second_string):
+    liststring = list(first_string)
+    i = 0
+    for index, letter in enumerate (liststring):
+        new_list = liststring[:index] + liststring[index+1 : ]
+        i += 1
+        if new_list == list(second_string):
+            return True
+        elif i == len(first_string):
+            return False
+print(near(input('first string please '),input('second string please ')))
