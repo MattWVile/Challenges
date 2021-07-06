ac3ffb6ab48adf8519267f14c6019cb6a2028f6f
MattWVile <matt.vile@yahoo.co.uk>
2021-07-06 11:04:07 +0000

    times tables function created

---
+++ times_tables.py
@@ -0,0 +1,14 @@
+def times_table(num):
+    total_count = 1
+    while total_count <= int(num):
+        result = []
+        nested_count = 1
+        while nested_count <= int(num):
+            result.append(total_count * nested_count)
+            nested_count += 1
+        if total_count == int(num):
+            return result
+        else:
+            print(result)
+            total_count += 1
+print(times_table(input('number please ')))
